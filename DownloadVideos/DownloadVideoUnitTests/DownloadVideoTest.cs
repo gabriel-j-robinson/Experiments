03cc2850cc27c68f84173fa93ecde0f2a5c3d034
Gabriel Robinson <109617870+gabrieljrobinsonx1@users.noreply.github.com>
2023-04-12 13:24:32 -0600

    neither method works

---
+++ DownloadVideos/DownloadVideoUnitTests/DownloadVideoTest.cs
@@ -0,0 +1,22 @@
+using DownloadVideosLibrary;
+
+namespace DownloadVideoUnitTests
+{
+  [TestClass]
+  public class DownloadVideoTest
+  {
+    [TestMethod]
+    public void TestDownloadVideoV1()
+    {
+      var task = Task.Run(() => DownloadVideo.DownloadVideoAsyncV1("blob:https://twitter.com/f9b663af-6a8c-4623-8214-1fb8bf9e65ba", "C:\\Users\\gabri\\Downloads\\video.mp4"));
+      task.Wait();
+    }
+
+    [TestMethod]
+    public void TestDownloadVideoV2()
+    {
+      var task = Task.Run(() => DownloadVideo.DownloadBlobVideoAsyncV2("blob:https://twitter.com/f9b663af-6a8c-4623-8214-1fb8bf9e65ba", "C:\\Users\\gabri\\Downloads\\video.mp4"));
+      task.Wait();
+    }
+  }
+}
---
+++ DownloadVideos/DownloadVideosLibrary/DownloadVideos.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace DownloadVideosLibrary
+{
+  public static class DownloadVideo
+  {
+    public static async Task DownloadVideoAsyncV1(string blobUrl, string outputPath)
+    {
+      using (var httpClient = new HttpClient())
+      {
+        var response = await httpClient.GetAsync(blobUrl);
+
+        if (!response.IsSuccessStatusCode)
+        {
+          throw new Exception($"Failed to download video: {response.StatusCode}");
+        }
+
+        using (var stream = await response.Content.ReadAsStreamAsync())
+        using (var outputStream = File.Open(outputPath, FileMode.Create))
+        {
+          await stream.CopyToAsync(outputStream);
+        }
+      }
+    }
+
+    public static async Task DownloadBlobVideoAsyncV2(string blobUrl, string outputPath)
+    {
+      if (!blobUrl.StartsWith("blob:", StringComparison.OrdinalIgnoreCase))
+      {
+        throw new ArgumentException("The URL is not a Blob URL.", nameof(blobUrl));
+      }
+
+      var blobData = Encoding.ASCII.GetBytes(blobUrl.Substring(5));
+
+      using (var stream = new MemoryStream(blobData))
+      using (var outputStream = File.Open(outputPath, FileMode.Create))
+      {
+        await stream.CopyToAsync(outputStream);
+      }
+    }
+  }
+}
